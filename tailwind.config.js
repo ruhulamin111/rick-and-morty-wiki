96b7cac428e2b2cdcfd37450a7d61a007653b7b2
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-12-07 22:02:58 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'white': '#fff',
+      'black': '#000',
+      'primary': '#14D9E5',
+      'secondary': '#9DFE00',
+      'tertiary': '#191D29',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
